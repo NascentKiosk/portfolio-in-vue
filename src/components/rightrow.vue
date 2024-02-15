11f6ba332e77b57ebe68a36a1287dbd0c5cd278c
k000832 <timothy.juma@smhi.se>
2024-02-15 16:09:45 +0100

    roles

---
+++ src/components/rightrow.vue
@@ -0,0 +1,141 @@
+<template lang="">
+    <div>
+    <div class=" bg-white border border-gray-200 p-6 sm:p-5 md:p-10 lg:p-14">
+      <h1 class="text-black text-2xl lg:text-3xl font-semi-bold leading-10">Tools / Frameworks</h1>
+      <div class="flex justify-between grid grid-cols-2 gap-2 m-5 mb-10">
+            <!-- START Card component -->
+            <div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/react-js-icon.svg" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>React.js</h5>
+                <p>5 Year exp</p>
+            </article>
+
+            </div>
+          
+  
+
+            <div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/nodejs.svg" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>Node.js</h5>
+                <p>5 Year exp</p>
+            </article>
+
+            </div>
+            <div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/vuejs-icon.svg" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>Vue.js</h5>
+                <p> 3 Year exp</p>
+            </article>
+
+            </div><div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/figma-icon.svg" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>Figma</h5>
+                <p>5 Year exp</p>
+            </article>
+
+            </div><div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/amazon_aws-icon.svg" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>AWS</h5>
+                <p> 3 Year exp</p>
+            </article>
+
+            </div><div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/getbootstrap-icon.svg" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>Bootstrap</h5>
+                <p>4 Year exp</p>
+            </article>
+            
+            </div>
+
+             <!-- START Card component -->
+             <div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/tailwindcss-icon.svg" alt="ReactJS Logo" class="h-[30px] w-[30px] rounded-50px">
+                <h5>Tailwind CSS</h5>
+                <p>5 Year exp</p>
+            </article>
+          </div>
+          <!-- START Card component -->
+          <div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/nextjs01.png" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>Next.js</h5>
+                <p>5 Year exp</p>
+            </article>
+          </div>
+        </div>
+        <br />
+      <div class="w-full border-t border-gray-300"></div>
+      <br />
+
+      <h1 class="text-black text-2xl lg:text-3xl font-semi-bold leading-10">Languages</h1>
+      <div class="flex justify-between grid grid-cols-2 gap-2 m-5 mb-10">
+        <!-- START Card component -->
+        <div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/javascript-icon.svg" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>JavaScript</h5>
+                <p>5 Year exp</p>
+            </article>
+          </div>
+          <!-- START Card component -->
+        <div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/typescriptlang-icon.svg" alt="ReactJS Logo" class="h-[30px] w-[30px] rounded-50px">
+                <h5>TypeScript</h5>
+                <p>5 Year exp</p>
+            </article>
+        </div>
+         <!-- START Card component -->
+        <div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/lOGO_c_sharp.svg" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>C#</h5>
+                <p>5 Year exp</p>
+            </article>
+        </div>
+         <!-- START Card component -->
+         <div  class="flex justify-center items-center ">
+              <article class="flex flex-col items-center bg-white shadow-2xl rounded-2xl p-5">
+                <img src="./img/logos/python-icon.svg" alt="ReactJS Logo" class="h-[30px] w-[50px] rounded-50px">
+                <h5>Python</h5>
+                <p>5 Year exp</p>
+            </article>
+        </div>
+
+        
+      </div>
+      <br />
+      <div class="w-full border-t border-gray-300"></div>
+      <br />
+      <h1 class="text-black text-2xl lg:text-3xl font-semi-bold leading-10">Education</h1> <br>
+      <div class="grid place-items-center">
+          <img src="./img/logos/jkuat.png" alt="ReactJS Logo" class="h-[80px] w-[80px] rounded-50px">
+          <br />
+          <h5 class="text-xs  font-semibold ">Jomo Kenyatta University Of Agriculture And Technology</h5>
+                <p>Diploma in Infrmation Technology</p>
+      </div>
+      
+      <br />
+                
+     
+     
+
+    </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
